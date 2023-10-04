49d939c0fda99be0f6b303c2a2bc598976bc8d41
Infocat <infocat2.0@gmail.com>
2023-10-04 18:46:06 -0500

    1.30

--- api/guardarPedido.php
+++ api/guardarPedido.php
@@ -5,18 +5,18 @@ $sql= $db->prepare("INSERT INTO `pedidos`(`idTour`, `idEstado`, `nacionalidad`, 
 `precAdulto`, `precMenor`, `total`, `moneda`, `tipoDocumento`, 
 `dni`, `nombre`, `apellido`, `correo`, `celular`, 
 `ciudad`, `direccion`, `titulo`, `separado`, tipoComprobante,
-nRuc, nRazon, nDireccion) VALUES
+nRuc, nRazon, nDireccion, horario) VALUES
 (?, ?, ?, ?, ?,
  ?, ?, ?, ?, ?,
  ?, ?, ?, ?, ?,
  ?, ?, ?, ?, ?,
- ?, ?, ?)");
+ ?, ?, ?, ?)");
 
 $resp = $sql->execute([ $_POST['id'], 1, $_POST['nacionalidad'], $_POST['adultos'], $_POST['kids'],
 	$_POST['adultoNormal'], $_POST['menorNormal'], $_POST['total'], $_POST['moneda'], $_POST['tipoDocumento'],
 	$_POST['documento'], $_POST['nombres'], $_POST['apellidos'], $_POST['correo'], $_POST['celular'],
 	$_POST['ciudad'], $_POST['direccion'], $_POST['titulo'], $_POST['empieza'], $_POST['tipoComprobante'],
-	$_POST['nRuc'], $_POST['nRazon'], $_POST['nDireccion']
+	$_POST['nRuc'], $_POST['nRazon'], $_POST['nDireccion'], 
  ]);
 
  if($resp){
